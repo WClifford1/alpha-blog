f95d12d45c74d6bd74f054431af9b27c56199b9d
William Clifford <williamdavidclifford@gmail.com>
2019-05-05 18:00:27 +1000

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  root "pages#home"
+
+  
+
+  resources :articles
+
   get 'pages/about'
   get 'pages/home'
   get "pages/home", to: "pages#home"
   get "pages/about", to: "pages#about"
+
 end
